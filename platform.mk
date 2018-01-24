4d308e272601c89b75c286d6dde9de494f115426
Offain <fabian.leutenegger@bluewin.ch>
2018-01-24 20:42:09 +0100

    jasmine: Adapt ANT+ for CAF
     * CAF wants it this way
    
    Change-Id: I178d2a9903432234b72d5794cc4ac41ff911e077

--- platform.mk
+++ platform.mk
@@ -59,11 +59,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	AntHalService \
 	antradio_app \
-	com.dsi.ant.antradio_library \
 	libantradio
-
-PRODUCT_COPY_FILES += \
-	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
