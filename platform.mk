83c81a3b1bd8a2ad720109b04a6b4624aeefb3f4
Offain <fabian.leutenegger@bluewin.ch>
2019-01-05 16:40:29 +0100

    jasmine: Revert back to 1.0 power HAL [1/2]
    Change-Id: I397346e3951e34b6c272fc7dee0b550508aee3b9

--- platform.mk
+++ platform.mk
@@ -319,7 +319,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-	android.hardware.power@1.1-service-qti
+        android.hardware.power@1.0-impl \
+        android.hardware.power@1.0-service
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
